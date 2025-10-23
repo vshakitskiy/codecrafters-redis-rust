7c90e874a1efcc517206e90972826f741d6d822e
vshakitskiy <vshakitskiy@gmail.com>
2025-10-23 16:35:05 +0300

    respond with multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
+use std::str::from_utf8;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
@@ -10,10 +11,23 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(stream: TcpStream) {
     println!("accepted new connection!");
+    let mut reader = BufReader::new(stream);
+    let mut line = String::new();
 
-    stream
-        .write_all(b"+PONG\r\n")
-        .expect("Failed to send a message");
+    while let Ok(n) = reader.read_line(&mut line) {
+        if n == 0 {
+            break;
+        }
+
+        if line.trim() == "PING" {
+            reader
+                .get_mut()
+                .write_all(b"+PONG\r\n")
+                .expect("Failed to send PONG");
+        }
+
+        line.clear();
+    }
 }
