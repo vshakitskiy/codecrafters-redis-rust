8dc9c08be9bf51bd18a5da119797002495e69141
vshakitskiy <vshakitskiy@gmail.com>
2025-10-23 15:54:53 +0300

    bind to a port

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        handle_client(stream.expect("Failed to accept incoming connection"));
+    }
+}
+
+fn handle_client(_stream: TcpStream) {
+    println!("accepted new connection!");
 }
